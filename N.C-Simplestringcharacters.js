de1d0c388c4b8e69237cf041a6db9d3d64ddf72a
Ramsey lewis <ramseylewis99@gmail.com>
2020-11-12 11:11:44 -0500

    code wars

--- N.C-Simplestringcharacters.js
+++ N.C-Simplestringcharacters.js
@@ -5,39 +5,31 @@
 // --the order is: uppercase letters, lowercase, numbers and special characters.
 
 
-// function solve(s){
-//     up = 0; low = 0; num = 0; spec = 0;
-//     Alphabet = 'A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z.';
+function solve(s){
 
-//     for(i = 0; i < s.length; i ++){
-//         // if s[i] == any upercaser letter up + 1
+    let arr= [0,0,0,0]
 
-//     }
-    
-// }
-
-
-console.log(findMissingLetter("*'&ABCDabcde12345"))
-
-
-function findMissingLetter(array){
-    let alpha = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'] 
-    let arrayLC = array.map(letter => letter.toLowerCase());
-    let start = alpha.indexOf(arrayLC[0])
-
-    for(let i = start; i < start + arrayLC.length + 1; i++){
-        let curentLet = alpha[i]
-    
-        if(!arrayLC.includes(curentLet)){
-            return array[0] === array[0].toLowerCase()?curentLet:curentLet.toUpperCase()
+    for(i = 0; i < s.length; i ++){
+        
+        if (s[i].match(/[A-Z]/g)) {
+            arr[0]++
+        } else  if (s[i].match(/[a-z]/g)) {
+            arr[1]++
+            
+        }else  if (s[i].match(/[0-9]/g)) {
+            arr[2]++
+        }else{
+            arr[3]++
         }
     }
+    return arr   
 }
 
 
-console.log(findMissingLetter("*'&ABCDabcde12345"))
-
-
-// https://www.codewars.com/users/RamseyJLewis/completed_solutions
-
-// link
+console.log(solve("*'&ABCDabcde1245"))
+console.log(solve("Codewars@codewars123.com"),[1,18,3,2]);
+console.log(solve("bgA5<1d-tOwUZTS8yQ"),[7,6,3,2]);
+console.log(solve("P*K4%>mQUDaG$h=cx2?.Czt7!Zn16p@5H"),[9,9,6,9]);
+console.log(solve("RYT'>s&gO-.CM9AKeH?,5317tWGpS<*x2ukXZD"),[15,8,6,9]);
+console.log(solve("$Cnl)Sr<7bBW-&qLHI!mY41ODe"), [10,7,3,6])
+console.log(solve("@mw>0=QD-iAx!rp9TaG?o&M%l$34L.nbft"), [7,13,4,10]);
