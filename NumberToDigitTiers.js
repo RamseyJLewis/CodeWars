89b25adbf3b4a24a351b8f1cf85b20a5498f250f
Ramsey lewis <ramseylewis99@gmail.com>
2021-01-03 19:00:37 -0500

    code wars

---
+++ NumberToDigitTiers.js
@@ -0,0 +1,24 @@
+// Create a function that takes a number and returns an array of strings containing the number cut off at each digit.
+
+// Examples
+// 420 should return ["4", "42", "420"]
+// 2017 should return ["2", "20", "201", "2017"]
+// 2010 should return ["2", "20", "201", "2010"]
+// 4020 should return ["4", "40", "402", "4020"]
+// 80200 should return ["8", "80", "802", "8020", "80200"]
+// PS: The input is guaranteed to be an integer in the range [0, 1000000]
+
+function createArrayOfTiers(num) {
+    let numStr = num.toString(); //'420'
+    let arr = []
+    let finalArr = []
+    for(let i = 0; i < numStr.length; i ++){
+       arr.push(numStr[i]) 
+       arr.join('')
+       console.log(arr)
+    }
+    return numStr;
+}
+
+console.log(createArrayOfTiers(420)); // 420 should return ["4", "42", "420"]
+// console.log(createArrayOfTiers(2017));
