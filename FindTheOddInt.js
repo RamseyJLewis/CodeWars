de7c31e9d9a85a042aefe4400213095e76e2f612
Ramsey lewis <ramseylewis99@gmail.com>
2021-01-17 18:05:38 -0500

    find the oddint

--- FindTheOddInt.js
+++ FindTheOddInt.js
@@ -5,27 +5,26 @@
 
 // There will always be only one integer that appears an odd number of times.
 
-function findOdd(A) {
-    let accum = []
-    let numSort = A.sort(function (a, b) { return b - a }) //puts numbers in order
-    for (let i = 0; i < numSort.length; i++) { // loops through each nbumber
-        if( numSort[i] == numSort[i+2]){ // if a number is repetaed 3 times 
-           accum.push(numSort[i], numSort[i+2])  
-          console.log(accum.pop());
-    
+function findOdd(arr) {
+    var result, num = 0;
+  
+    arr = arr.sort();
+    for (var i = 0; i < arr.length; i++) {
+      if (arr[i] === arr[i+1]) {
+        num++;
+      } else {
+        num++;
+        if (num % 2 != 0) {
+          result = arr[i];
+          break;
         }
-
+      }
     }
-    // return 0;
-}
+    return result;
+  }
 
 console.log(findOdd([20, 1, -1, 2, -2, 3, 3, 5, 5, 1, 2, 4, 20, 4, -1, -2, 5, 12])); //5
 console.log([1,1,1,1,1,1,10,1,1,1,1]); //10
 
 
 
-// [
-//     20, 20,  5, 5, 5, 4,  4,
-//      3,  3,  2, 2, 1, 1, -1,
-//     -1, -2, -2
-//   ]
