ee2de3a6300191de8a149e5ef5204b7f9a2b0304
Ramsey lewis <ramseylewis99@gmail.com>
2021-01-18 13:51:24 -0500

    code wars shortest word solved

--- ShortestWord.js
+++ ShortestWord.js
@@ -1,7 +1,13 @@
 function findShort(s){
-
-
+let arr = s.split(' ');
+let accum = [];
+for(let i=0; i<arr.length; i++){
+    accum.push(arr[i].length);
+    accum.sort(function(a, b){return a-b});
+}
+ return accum[0];
 }
 
-
-console.log(findShort("turns out random test cases are easier than writing out basic ones"), 3); 
+// console.log(findShort("turns out random test cases are easier than writing out basic ones"))   //, 3); 
+console.log(findShort("bitcoin take over the world maybe who knows perhaps"))
+                         
