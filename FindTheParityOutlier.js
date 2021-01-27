27f9ad1a6ea95b6a08adceac9f2a41a24cba846f
Ramsey lewis <ramseylewis99@gmail.com>
2021-01-27 10:37:59 -0500

    code wars findtheparity test

---
+++ FindTheParityOutlier.js
@@ -0,0 +1,43 @@
+// You are given an array (which will have a length of at least 3, but could be very large) containing integers. The array is either entirely comprised of odd integers or entirely comprised of even integers except for a single integer N. Write a method that takes the array as an argument and returns this "outlier" N.
+
+// [2, 4, 0, 100, 4, 11, 2602, 36]
+// Should return: 11 (the only odd number)
+
+// [160, 3, 1719, 19, 11, 13, -21]
+// Should return: 160 (the only even number)
+
+function findOutlier(integers) {
+    //loop through
+    let odd = 0
+    let even = 0
+    let output = 0
+    for (let i = 0; i < integers.length; i++ ) {
+        if (integers[i] % 2 == 0) {
+            even++            
+        } else{
+            odd++
+        }
+    }
+    if (odd > even) {
+        for (i = 0; i < integers.length; i++) {
+            if (integers[i] % 2 == 0 ) {
+                output = integers[i]
+            }
+        }
+    } else if (even > odd) {
+        for (i = 0; i < integers.length; i++) {
+            if (integers[i] % 2 !== 0) {
+                output = integers[i]
+            }
+        }
+    }
+    return output
+}
+
+
+
+console.log(findOutlier([0, 1, 2]))//, 1)
+console.log(findOutlier([1, 2, 3]))//, 2)
+console.log(findOutlier([2,6,8,10,3]))//, 3)
+console.log(findOutlier([0,0,3,0,0]))//, 3)
+console.log(findOutlier([1,1,0,1,1]))//, 0)
