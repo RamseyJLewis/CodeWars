3be65281347b82f3c62a774c8b5ae0a46d34fd83
Ramsey lewis <ramseylewis99@gmail.com>
2021-02-17 01:56:54 -0500

    code wars stat ath associ

--- Statistics for an Athletic Association.js
+++ Statistics for an Athletic Association.js
@@ -26,15 +26,23 @@
 
 
 function stat(strg) {
+let eachAth = strg.split(' ')
+for(let i = 0; i < eachAth.length; i++){
+    if(eachAth[i][0] == 0){
+        eachAth[i] = eachAth[i].slice(1)
+    }
+    
+    // to get the range compare the value of 
+}
+return console.log(eachAth);
 
-//spert
 }
 
 
 
     console.log(stat("01|15|59, 1|47|16, 01|17|20, 1|32|34, 2|17|17"))
         //"Range: 01|01|18 Average: 01|38|05 Median: 01|32|34")
-    console.log(stat("02|15|59, 2|47|16, 02|17|20, 2|32|34, 2|17|17, 2|22|00, 2|31|41"))
+   // console.log(stat("02|15|59, 2|47|16, 02|17|20, 2|32|34, 2|17|17, 2|22|00, 2|31|41"))
         //"Range: 00|31|17 Average: 02|26|18 Median: 02|22|00")
     
     
